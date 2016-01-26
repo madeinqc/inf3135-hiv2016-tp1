19798e200aa91b7291422edf8fb1f23eff39d32d
Marc-Antoine Sauve <madeinqchw@gmail.com>
2016-01-25 19:57:43 -0500

    Ajout de la lecture des villes, du tri et de l'affichage des résultats
    Signed-off-by: Marc-Antoine Sauve <madeinqchw@gmail.com>

--- README.md
+++ README.md
@@ -21,12 +21,56 @@ Le logiciel s'utilise comme ceci:
 ```
 Où *<nombre de villes>* est un chiffre entre 1 et 5000 et représente le nombre de villes à afficher.
 
+### Exemple 1
+
+```
+./tp1 20
+Rang   Nom                         Pays                        Population
+----   ---                         ----                        ----------
+   1   Shanghai                    China                         22315474
+   2   Buenos Aires                Argentina                     13076300
+   3   Mumbai                      India                         12691836
+   4   Mexico City                 Mexico                        12294193
+   5   Beijing                     China                         11716620
+   6   Karachi                     Pakistan                      11624219
+   7   Istanbul                    Turkey                        11174257
+   8   Tianjin                     China                         11090314
+   9   Guangzhou                   China                         11071424
+  10   Delhi                       India                         10927986
+  11   Manila                      Philippines                   10444527
+  12   Moscow                      Russia                        10381222
+  13   Shenzhen                    China                         10358381
+  14   Dhaka                       Bangladesh                    10356500
+  15   Seoul                       South Korea                   10349312
+  16   Sao Paulo                   Brazil                        10021295
+  17   Wuhan                       China                          9785388
+  18   Lagos                       Nigeria                        9000000
+  19   Jakarta                     Indonesia                      8540121
+  20   Tokyo                       Japan                          8336599
+```
+
+### Exemple 2
+
+```
+./tp1 test
+Erreur: Vous devez fournir un nombre entre 1 et 5000 comme argument.
+
+Usage:
+	./tp1 <nombre de villes>
+
+Où <nombre de ville> doit être un entier entre 1 et 5000.
+```
+
 ## Contenu du projet
 
 tp1.c - Code source du logiciel.
 README.md - Ce fichier, une description du projet écrit en markdown.
-Makefile - Fichier de configuration pour make.
+Makefile - Fichier de configuration pour make. Ce fichier contient les instructions nécessaires pour compiler le logiciel, pour nettoyer le dossier ainsi que pour télécharger les données nécessaire à l'exécution.
+
+## Références
+
+[Quicksort - Wikipedia, the free encyclopedia](https://en.wikipedia.org/wiki/Quicksort) - Le pseudocode à été utiliser pour écrire l'algorythme de tri rapide.
 
 ## Status
 
-Le projet est en développement et peut ne pas fonctionner ou contenir des bogues.
+Le projet est terminé et fonctionnel. Aucun bogue détecté.
--- tp1.c
+++ tp1.c
@@ -12,27 +12,31 @@
 #define NOMBRE_VILLE_MAXIMUM 25000
 #define LONG_LIGNE 11000
 #define FICHIER_PAYS "data/countryInfo.txt"
+#define FICHIER_VILLE "data/cities15000.txt"
 
 // Les structures et constantes associées suivantes (Pays et Ville) sont inspirées de l'énoncé du travail disponible à http://thales.math.uqam.ca/~blondin/fr/inf3135-tp1
-typedef struct Pays {
+struct Pays {
   char nom[LONG_NOM_PAYS]; // Le nom ASCII
   char codeISO[CODE_ISO_PAYS]; // Le code ISO du pays
-} Pays;
-const Pays PAYS_BIDON = {"?", "?"};
+};
 
-typedef struct Ville {
+struct Ville {
   char nom[LONG_NOM_VILLE]; // Le nom ASCII
   long population; // La population
-  Pays pays; // Le pays de la ville
-} Ville;
-const Ville VILLE_BIDON = {"?", -1, {"?", "?"}};
+  struct Pays pays; // Le pays de la ville
+};
 
-void printError(char* msg);
-void printUsage(char* nomDuProgramme);
-int obtenirPays(Pays* pays);
-int extractionPays(Pays *pays, char* line);
-int obtenirVille(Ville* villes);
-int extractionVille(Ville *ville, char* line);
+void printError(char *msg);
+void printUsage(char *nomDuProgramme);
+int obtenirPays(struct Pays *pays);
+int extractionPays(struct Pays *pays, char *line);
+void passerTokens(char **line, int nombre, char *token);
+int obtenirVille(struct Ville* villes, struct Pays pays[], int nombrePays);
+int extractionVille(struct Ville *ville, char* line, struct Pays pays[], int nombrePays);
+void remplirPays(struct Ville *ville, char *codeISO, struct Pays pays[], int nombrePays);
+void trierRapidement(struct Ville tab[], int longTab);
+void trierRapidementRec(struct Ville tab[], int i, int j);
+int partitionner(struct Ville tab[], int i, int j);
 
 int main(int argc, char** argv) {
   if (argc != 2) {
@@ -41,17 +45,28 @@ int main(int argc, char** argv) {
     return ARG_COUNT_ERROR;
   }
 
-  int count = atoi(argv[1]);
-  if (count < 1 || count > 5000) {
+  char *fin;
+  long count = strtol(argv[1], &fin, 10);
+  if (*fin != 0 || count < 1 || count > 5000) {
     printError("Vous devez fournir un nombre entre 1 et 5000 comme argument.");
     printUsage(argv[0]);
     return ARG_RANGE_ERROR;
   }
 
-  Pays pays[NOMBRE_PAYS_MAXIMUM];
+  struct Pays pays[NOMBRE_PAYS_MAXIMUM];
   int nombrePays = obtenirPays(pays);
 
-  // TODO Implémenter la fonctionnalité principale
+  struct Ville villes[NOMBRE_VILLE_MAXIMUM];
+  int nombreVilles = obtenirVille(villes, pays, nombrePays);
+
+  trierRapidement(villes, nombreVilles);
+
+  printf("%4s   %-25s   %-25s   %10s\n", "Rang", "Nom", "Pays", "Population");
+  printf("%4s   %-25s   %-25s   %10s\n", "----", "---", "----", "----------");
+  int i;
+  for (i = 0; i < count && i < nombreVilles; i++) {
+    printf("%4d   %-25.25s   %-25.25s   %10d\n", i+1, villes[i].nom, villes[i].pays.nom, villes[i].population);
+  }
 
   return 0;
 }
@@ -61,11 +76,11 @@ int main(int argc, char** argv) {
  * @param pays Le pointeur qui sera populé avec la liste des pays.
  * @return Nombre de pays chargé ou le numéro d'erreur en cas d'échec.
  */
-int obtenirPays(Pays pays[]) {
+int obtenirPays(struct Pays pays[]) {
   FILE* file = fopen(FICHIER_PAYS, "r");
   if (!file) {
-    char message[MESSAGE_ERREUR_MAXIMUM];
-    sprintf(message, "Impossible d'ouvrir le fichier %s", FICHIER_PAYS);
+    char message[MESSAGE_ERREUR_MAXIMUM] = "Impossible d'ouvrir le fichier ";
+    strncat(message, FICHIER_PAYS, MESSAGE_ERREUR_MAXIMUM);
     printError(message);
     return COULD_NOT_OPEN_FILE;
   }
@@ -73,7 +88,7 @@ int obtenirPays(Pays pays[]) {
   char line[LONG_LIGNE];
   int nombrePaysLus = 0;
   
-  while (fgets(line, LONG_LIGNE, file) != NULL) {
+  while (fgets(line, LONG_LIGNE, file) != NULL && nombrePaysLus < NOMBRE_PAYS_MAXIMUM) {
     if (extractionPays(&pays[nombrePaysLus], line)) ++nombrePaysLus;
   }
 
@@ -87,25 +102,87 @@ int obtenirPays(Pays pays[]) {
  * @param pays Le pointeur vers un Pays qui sera populé.
  * @param line La chaîne de caractère contenant l'information à extraire.
  */
-int extractionPays(Pays *pays, char* line) {
+int extractionPays(struct Pays *pays, char* line) {
   char *token;
 
-  if (line[0] == '#')
-    return 0;
+  if (line[0] == '#') return 0;
 
   token = strsep(&line, "\t");
-  if (line == NULL) return 0;
-  strcpy(pays->codeISO, token);
+  if (token == NULL) return 0;
+  strncpy(pays->codeISO, token, CODE_ISO_PAYS - 1);
+  pays->codeISO[CODE_ISO_PAYS - 1] = 0;
 
-  strsep(&line, "\t");
-  strsep(&line, "\t");
-  strsep(&line, "\t");
+  passerTokens(&line, 3, "\t");
   
   token = strsep(&line, "\t");
-  if (line == NULL) return 0;
-  strcpy(pays->nom, token);
+  if (token == NULL) return 0;
+  strncpy(pays->nom, token, LONG_NOM_PAYS - 1);
+  pays->nom[LONG_NOM_PAYS - 1] = 0;
 
   return 1;
+}
+
+void passerTokens(char **line, int nombre, char *token) {
+  int i;
+  for (i = 0; i < nombre; i++) strsep(line, token);
+}
+
+int obtenirVille(struct Ville* villes, struct Pays pays[], int nombrePays) {
+  FILE* file = fopen(FICHIER_VILLE, "r");
+  if (!file) {
+    char message[MESSAGE_ERREUR_MAXIMUM] = "Impossible d'ouvrir le fichier ";
+    strncat(message, FICHIER_VILLE, MESSAGE_ERREUR_MAXIMUM);
+    printError(message);
+    return COULD_NOT_OPEN_FILE;
+  }
+
+  char line[LONG_LIGNE];
+  int nombreVillesLus = 0;
+  
+  while (fgets(line, LONG_LIGNE, file) != NULL && nombreVillesLus < NOMBRE_VILLE_MAXIMUM) {
+    if (extractionVille(&villes[nombreVillesLus], line, pays, nombrePays)) ++nombreVillesLus;
+  }
+
+  fclose(file);
+
+  return nombreVillesLus;
+}
+
+int extractionVille(struct Ville *ville, char* line, struct Pays pays[], int nombrePays) {
+  char *token;
+
+  passerTokens(&line, 2, "\t");
+
+  token = strsep(&line, "\t");
+  if (token == NULL) return 0;
+  strncpy(ville->nom, token, LONG_NOM_VILLE - 1);
+  ville->nom[LONG_NOM_VILLE - 1] = 0;
+
+  passerTokens(&line, 5, "\t");
+
+  token = strsep(&line, "\t");
+  if (token == NULL) return 0;
+
+  remplirPays(ville, token, pays, nombrePays);
+  
+  passerTokens(&line, 5, "\t");
+  
+  token = strsep(&line, "\t");
+  if (token == NULL) return 0;
+  char *fin;
+  ville->population = strtol(token, &fin, 10);
+
+  return 1;
+}
+
+void remplirPays(struct Ville *ville, char *codeISO, struct Pays pays[], int nombrePays) {
+  int i;
+  for (i = 0; i < nombrePays; i++) {
+    if (strncmp(codeISO, pays[i].codeISO, 2) == 0) {
+      ville->pays = pays[i];
+      return;
+    }
+  }
 }
 
 /**
@@ -121,5 +198,36 @@ void printError(char* msg) {
  * @param char* Nom du programme.
  */
 void printUsage(char* nomDuProgramme) {
-  printf("Usage:\n\t./%s <nombre de villes>\n\nOù <nombre de ville> doit être un entier entre 1 et 5000.\n", nomDuProgramme);
+  printf("Usage:\n\t%s <nombre de villes>\n\nOù <nombre de ville> doit être un entier entre 1 et 5000.\n", nomDuProgramme);
+}
+
+void trierRapidement(struct Ville tab[], int longTab) {
+  trierRapidementRec(tab, 0, longTab-1);
+}
+
+void trierRapidementRec(struct Ville tab[], int i, int j) {
+  int pivot;
+  if (i < j) {
+    pivot = partitionner(tab, i, j);
+    trierRapidementRec(tab, i, pivot - 1);
+    trierRapidementRec(tab, pivot + 1, j);
+  }
+}
+
+int partitionner(struct Ville tab[], int i, int j) {
+  struct Ville pivot = tab[j];
+  int min = i;
+  int max;
+  for (max = i; max < j; max++) {
+    if (tab[max].population >= pivot.population) {
+      struct Ville temp = tab[min];
+      tab[min] = tab[max];
+      tab[max] = temp;
+      min++;
+    }
+  }
+  struct Ville temp = tab[min];
+  tab[min] = tab[j];
+  tab[j] = temp;
+  return min;
 }
